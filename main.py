fd000341cc3ef6c1d4b35c25dc538e0049039954
William Denniss <wdenniss@google.com>
2015-06-19 11:08:03 -0700

    Added audience validation check to protect against a confused deputy attack.
    This is an attack where the publisher of another app has a legitimately obtained access token for the user for *their* app, and attempts to use that access token to access our app on behalf of that user. Checking the audience ensures that only tokens issued to the user in the context of *our* app can be used.

--- main.py
+++ main.py
@@ -16,13 +16,19 @@ class MainHandler(webapp2.RequestHandler):
     self.response.write('\noauth.get_current_user(%s)' % repr(scope))
     try:
       user = oauth.get_current_user(scope)
+      allowed_clients = ['407408718192.apps.googleusercontent.com'] # list your client ids here
+      token_audience = oauth.get_client_id(scope)
+      if token_audience not in allowed_clients:
+        raise oauth.OAuthRequestError('audience of token \'%s\' is not in allowed list (%s)'
+                                      % (token_audience, allowed_clients))
+
       self.response.write(' = %s\n' % user)
       self.response.write('- auth_domain = %s\n' % user.auth_domain())
       self.response.write('- email       = %s\n' % user.email())
       self.response.write('- nickname    = %s\n' % user.nickname())
       self.response.write('- user_id     = %s\n' % user.user_id())
     except oauth.OAuthRequestError, e:
-      self.response.set_status(200)
+      self.response.set_status(401)
       self.response.write(' -> %s %s\n' % (e.__class__.__name__, e.message))
       logging.warn(traceback.format_exc())
 
